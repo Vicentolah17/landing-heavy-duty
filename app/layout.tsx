d7bd96bf824b6b28721824c878836d970328a34c
Vicentolah17 <vivanetolopes@gmail.com>
2026-04-25 02:25:29 -0300

    adicionando pixel meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className={`${bebasNeue.variable} ${inter.variable} scroll-smooth`}>
       <head>
+        {/* Google Analytics 4 */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-2JEK8J9610"
           strategy="afterInteractive"
@@ -53,6 +54,31 @@ export default function RootLayout({
             gtag('config', 'G-2JEK8J9610');
           `}
         </Script>
+
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1473071244351456');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{display:'none'}}
+            src="https://www.facebook.com/tr?id=1473071244351456&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </head>
       <body className="font-sans antialiased bg-background text-foreground">
         {children}
